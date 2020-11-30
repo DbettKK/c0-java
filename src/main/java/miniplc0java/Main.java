0ac5fd74314589e2da99c515bf29e5088ccac8c6
DbettKK <394739138@qq.com>
2020-11-30 18:26:20 +0800

    nihao

--- src/main/java/miniplc0java/Main.java
+++ src/main/java/miniplc0java/Main.java
@@ -6,22 +6,24 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
+    public static void getData(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String str;
+        while ( (str=reader.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream(new File(args[0]));
+        File file = new File(args[0]);
+        FileInputStream inputStream = new FileInputStream(file);
         ANTLRInputStream input = new ANTLRInputStream(inputStream);
         scratchLexer lex = new scratchLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lex);
         scratchParser parser = new scratchParser(tokens);
-        try {
-            System.out.println(parser.program().toStringTree(parser));
-        }
-        catch (RecognitionException e) {
-            e.printStackTrace();
-        }
+        getData(file);
+        System.out.println(parser.program().toStringTree(parser));
     }
 }
